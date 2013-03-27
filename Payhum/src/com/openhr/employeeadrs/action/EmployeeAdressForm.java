68bec1513067d00635243543835473caf0a55dc8
Admin <Admin@sys>
2013-03-27 16:00:48 +0530

    report actions for Employee Adress

---
+++ Payhum/src/com/openhr/employeeadrs/action/EmployeeAdressForm.java
@@ -0,0 +1,46 @@
+package com.openhr.employeeadrs.action;
+
+import org.apache.struts.action.ActionForm;
+
+public class EmployeeAdressForm extends ActionForm
+
+{
+	private Integer id;
+	 private String employeeId;
+	 private String firstname;
+	 private String middlename;
+	 private String adress;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getMiddlename() {
+		return middlename;
+	}
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	 
+	 
+
+}
